136aef7397abb2a1fdd6a485af270d2454b76ac8
Gina White <ginabythebay@gmail.com>
2026-09-18 16:41:28 +0200

    use if let to avoid some intermediate checks

--- excercism/rust/protein-translation/src/lib.rs
+++ excercism/rust/protein-translation/src/lib.rs
@@ -1,20 +1,23 @@
 pub fn translate(rna: &str) -> Option<Vec<&str>> {
     let mut accum = Vec::new();
     let mut remaining: &str = rna;
-    let mut stop = false;
-    while remaining.len() > 2 && !stop {
-        let next = &remaining[..3];
-        remaining = &remaining[3..];
-        match next {
-            "AUG" => accum.push("Methionine"),
-            "UUU" | "UUC" => accum.push("Phenylalanine"),
-            "UUA" | "UUG" => accum.push("Leucine"),
-            "UCU" | "UCC" | "UCA" | "UCG" => accum.push("Serine"),
-            "UAU" | "UAC" => accum.push("Tyrosine"),
-            "UGU" | "UGC" => accum.push("Cysteine"),
-            "UGG" => accum.push("Tryptophan"),
-            "UAA" | "UAG" | "UGA" => stop = true,
-            _ => return None,
+    loop {
+        let result = remaining.get(..3);
+        if let Some(next) = result {
+            remaining = &remaining[3..];
+            match next {
+                "AUG" => accum.push("Methionine"),
+                "UUU" | "UUC" => accum.push("Phenylalanine"),
+                "UUA" | "UUG" => accum.push("Leucine"),
+                "UCU" | "UCC" | "UCA" | "UCG" => accum.push("Serine"),
+                "UAU" | "UAC" => accum.push("Tyrosine"),
+                "UGU" | "UGC" => accum.push("Cysteine"),
+                "UGG" => accum.push("Tryptophan"),
+                "UAA" | "UAG" | "UGA" => break,
+                _ => return None,
+            }
+        } else {
+            break;
         }
     }
 
